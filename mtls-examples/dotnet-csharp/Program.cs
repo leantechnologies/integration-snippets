71aef15f42dc0850cc0a0afb7ec626621cda5a0d
Amr Essam <amroessams@gmail.com>
2022-03-01 19:19:09 +0400

    Added dotnet c# example

---
+++ mtls-examples/dotnet-csharp/Program.cs
@@ -0,0 +1,48 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace HTTP_Test
+{
+  class program
+  {
+    static void Main()
+    {
+      Task t = new Task(HTTP_GET);
+      t.Start();
+      Console.ReadLine();
+    }
+
+
+    static async void HTTP_GET()
+    {
+      // path of pfx file generate by 
+      // $ openssl pkcs12 -export -out certificate.pfx -inkey privateKey.key -in certificate.crt -certfile CAcert 
+      var certificateLocation = "";
+      // password of pfx file
+      var certificatePassword = "";
+      // lean app token
+      var leanAppToken = "";
+      // create a new HttpClientHandler
+      var handler = new HttpClientHandler();
+      // create a new certficite using location and password
+      var certificate = new X509Certificate2(certificateLocation, certificatePassword);
+      // add certificate to client certificates
+      handler.ClientCertificates.Add(certificate);
+      // create a new HttpClient using the handler and setting the default BaseAddress as https://api.leantech.me/
+      var client = new HttpClient(handler)
+      {
+        BaseAddress = new Uri("https://api.leantech.me/")
+      };
+      // add lea app token default request header
+      client.DefaultRequestHeaders.Add("lean-app-token", leanAppToken);
+
+      // request the banks endpoint
+      HttpResponseMessage response = await client.GetAsync("/banks/v1");
+
+      // output to console status and JSON
+      Console.WriteLine("Response StatusCode: " + (int)response.StatusCode);
+      HttpContent content = response.Content;
+      string result = await content.ReadAsStringAsync();
+      Console.WriteLine(result);
+    }
+  }
+}
